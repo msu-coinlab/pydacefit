e4ba028b3be63220b6743884bbe624b62b3cfa6f
Julian <jules89@arcor.de>
2019-03-27 14:32:32 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ def readme():
 __name__ = "pydacefit"
 __author__ = "Julian Blank"
 __version__ = '1.0.0'
-__url__ = "https://github.com/julesy89/pydacefit"
+__url__ = "https://github.com/msu-coinlab/pydacefit"
 
 setup(
     name=__name__,
     version=__version__,
     author=__author__,
     author_email="blankjul@egr.msu.edu",
-    description="Sampling",
+    description="Surrogate Toolbox for Python",
     long_description=readme(),
     url=__url__,
     license='Apache License 2.0',
-    keywords="optimization",
+    keywords="metamodel, surrogate, response surface",
     install_requires=['numpy'],
     packages=find_packages(exclude=['tests', 'docs']),
     include_package_data=True,
